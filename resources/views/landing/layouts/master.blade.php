363ebcbbd16b50e56994bb9000139dae2fdea408
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-13 12:40:24 +0100

    Update master.blade.php

--- resources/views/landing/layouts/master.blade.php
+++ resources/views/landing/layouts/master.blade.php
@@ -14,6 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet">
 
     <!--====== Vendor Css ======-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('assets/ludus/css/vendor.css') }} ">
 
     <!--====== Utility-Spacing ======-->
