cf96e40ed0b947dffd118c64b3c844d6487ceb4f
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-19 17:38:39 +0100

    add carts section in navbar blade

---
+++ resources/views/landing/layouts/cart/CartNavContent.blade.php
@@ -0,0 +1,140 @@
+<div class="mini-cart">
+
+    <!--====== Mini Product Container ======-->
+    <div class="mini-product-container gl-scroll u-s-m-b-15">
+
+        <!--====== Card for mini cart ======-->
+        <div class="card-mini-product">
+            <div class="mini-product">
+                <div class="mini-product__image-wrapper">
+
+                    <a class="mini-product__link" href="#">
+
+                        <img class="u-img-fluid"
+                            src="{{ asset('assets/ludus/images/product/electronic/product3.jpg') }}"
+                            alt=""></a>
+                </div>
+                <div class="mini-product__info-wrapper">
+
+                    <span class="mini-product__category">
+
+                        <a href="#">Electronics</a></span>
+
+                    <span class="mini-product__name">
+
+                        <a href="#">Yellow Wireless
+                            Headphone</a></span>
+
+                    <span class="mini-product__quantity">1 x</span>
+
+                    <span class="mini-product__price">$8</span>
+                </div>
+            </div>
+
+            <a class="mini-product__delete-link far fa-trash-alt"></a>
+        </div>
+        <!--====== End - Card for mini cart ======-->
+
+
+        <!--====== Card for mini cart ======-->
+        <div class="card-mini-product">
+            <div class="mini-product">
+                <div class="mini-product__image-wrapper">
+
+                    <a class="mini-product__link" href="#">
+
+                        <img class="u-img-fluid"
+                            src="{{ asset('assets/ludus/images/product/electronic/product18.jpg') }} "
+                            alt=""></a>
+                </div>
+                <div class="mini-product__info-wrapper">
+
+                    <span class="mini-product__category">
+
+                        <a href="#">Electronics</a></span>
+
+                    <span class="mini-product__name">
+
+                        <a href="#">Nikon DSLR Camera 4k</a></span>
+
+                    <span class="mini-product__quantity">1 x</span>
+
+                    <span class="mini-product__price">$8</span>
+                </div>
+            </div>
+
+            <a class="mini-product__delete-link far fa-trash-alt"></a>
+        </div>
+        <!--====== End - Card for mini cart ======-->
+
+
+        <!--====== Card for mini cart ======-->
+        <div class="card-mini-product">
+            <div class="mini-product">
+                <div class="mini-product__image-wrapper">
+
+                    <a class="mini-product__link" href="#">
+
+                        <img class="u-img-fluid"
+                            src="images/product/women/product8.jpg"
+                            alt=""></a>
+                </div>
+                <div class="mini-product__info-wrapper">
+
+                    <span class="mini-product__category">
+
+                        <a href="#">Women Clothing</a></span>
+
+                    <span class="mini-product__name">
+
+                        <a href="#">New Dress D Nice
+                            Elegant</a></span>
+
+                    <span class="mini-product__quantity">1 x</span>
+
+                    <span class="mini-product__price">$8</span>
+                </div>
+            </div>
+
+            <a class="mini-product__delete-link far fa-trash-alt"></a>
+        </div>
+        <!--====== End - Card for mini cart ======-->
+        <!--====== Card for mini cart ======-->
+        <div class="card-mini-product">
+            <div class="mini-product">
+                <div class="mini-product__image-wrapper">
+                    <a class="mini-product__link" href="#">
+                        <img class="u-img-fluid"
+                            src="{{ asset('assets/ludus/images/product/men/product8.jpg') }} "
+                            alt=""></a>
+                </div>
+                <div class="mini-product__info-wrapper">
+                    <span class="mini-product__category">
+                        <a href="#">Men Clothing</a></span>
+                    <span class="mini-product__name">
+                        <a href="#">New Fashion D Nice
+                            Elegant</a></span>
+                    <span class="mini-product__quantity">1 x</span>
+                    <span class="mini-product__price">$8</span>
+                </div>
+            </div>
+            <a class="mini-product__delete-link far fa-trash-alt"></a>
+        </div>
+        <!--====== End - Card for mini cart ======-->
+    </div>
+    <!--====== End - Mini Product Container ======-->
+    <!--====== Mini Product Statistics ======-->
+    <div class="mini-product-stat">
+        <div class="mini-total">
+            <span class="subtotal-text">SUBTOTAL</span>
+            <span class="subtotal-value">$16</span>
+        </div>
+        <div class="mini-action">
+            <a class="mini-link btn--e-brand-b-2" href="#">PROCEED TO
+                CHECKOUT</a>
+            <a class="mini-link btn--e-transparent-secondary-b-2" href="{{route('cart.list')}}">VIEW
+                CART</a>
+        </div>
+    </div>
+    <!--====== End - Mini Product Statistics ======-->
+</div>
---
+++ resources/views/landing/layouts/cart/CartNavCount.blade.php
@@ -0,0 +1,2 @@
+<a class="mini-cart-shop-link"><i class="fas fa-shopping-bag"></i>
+    <span class="total-item-round">5</span></a>
