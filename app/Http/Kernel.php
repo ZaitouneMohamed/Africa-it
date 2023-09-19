de46e8fb63006ded3e024376194f2e62a63a84ec
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-19 22:24:25 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'AdminRedirection' => \App\Http\Middleware\AdminRedirectionIfNotAuth::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
