a1fb7889c5bc07c03ce85cbe5df7e47b2a1706a6
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-19 13:22:36 +0100

    send variables from controller to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Categorie;
+use App\Models\Product;
+use App\Models\SubCategorie;
 
 class HomeController extends Controller
 {
     function index()
     {
-        return view('landing.home');
+        $last_categories = Categorie::latest()->take(4)->get();
+        $prenium_product = Product::Prenium()->get();
+        $last_product = Product::latest()->get();
+        $subCategories = SubCategorie::latest()->get();
+        return view('landing.home')->with([
+            "lastCategories" => $last_categories,
+            "prenium_product" => $prenium_product,
+            "last_products" => $last_product,
+            "subCategories" => $subCategories
+        ]);
     }
 }
