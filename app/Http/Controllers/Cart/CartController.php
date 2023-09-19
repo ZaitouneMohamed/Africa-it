271250df077b30a8d97ed0ce348b8db38b758fd0
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-19 17:38:55 +0100

    Create CartController.php

---
+++ app/Http/Controllers/Cart/CartController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Cart;
+
+use App\Http\Controllers\Controller;
+use App\Models\Branch;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function cartlist()
+    {
+        $cart = session()->get('cart', []);
+        // dd($cart);
+        return view('landing.Cart',compact("cart"));
+    }
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+        $cart = session()->get('cart', []);
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "id" => $product->id,
+                // "image" => $product->FirstImage,
+                "title" => $product->title,
+                "quantity" => 1,
+                "price" => $product->price,
+            ];
+        }
+        session()->put('cart', $cart);
+    }
+    public function updateCart(Request $request)
+    {
+        if ($request->id && $request->quantity) {
+            $cart = session()->get('cart');
+            $cart[$request->id]["quantity"] = $request->quantity;
+            session()->put('cart', $cart);
+            session()->flash('success', 'Product added to cart.');
+        }
+    }
+
+    public function getCartCount()
+    {
+        $total = 0;
+        $count = 0;
+        foreach (session('cart') as $id => $item) {
+            $total += $item['price'] * $item['quantity'];
+            $count++;
+        };
+        if ($count === 0) {
+            $count = 0;
+        }
+        return response()->json([
+            "total" => $total,
+            "subtotal" => $total + 30,
+            "count" => $count
+        ]);
+    }
+
+    public function CheckOut()
+    {
+        $branches = Branch::all();
+        return view('electro.checkout',compact("branches"));
+    }
+
+    public function getCartContent()
+    {
+        if (session('cart')) {
+            $cartItems = array_values(session('cart'));
+            return response()->json($cartItems);
+        } else {
+            return response()->json([]);
+        }
+    }
+
+    public function deleteProduct(Request $request)
+    {
+        if ($request->id) {
+            $cart = session()->get('cart');
+            if (isset($cart[$request->id])) {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            session()->flash('success', 'Product successfully deleted.');
+        }
+    }
+}
