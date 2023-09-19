62a5a872ace3ea1f8700d628d212499af6b46207
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-19 18:21:19 +0100

    Update CartNavCount.blade.php

--- resources/views/landing/layouts/cart/CartNavCount.blade.php
+++ resources/views/landing/layouts/cart/CartNavCount.blade.php
@@ -1,2 +1,21 @@
 <a class="mini-cart-shop-link"><i class="fas fa-shopping-bag"></i>
-    <span class="total-item-round">5</span></a>
+    <span class="total-item-round" id="CartCount"></span></a>
+
+<script>
+    function CartCount() {
+        $.ajax({
+            type: 'GET',
+            url: "getCartCount",
+            success: function(response) {
+                console.log(response.count);
+                document.getElementById('CartCount').innerHTML = response.count
+            },
+            error: function() {
+                console.log('An error occurred .');
+            }
+        })
+    }
+    setInterval(() => {
+        CartCount();
+    }, 100);
+</script>
