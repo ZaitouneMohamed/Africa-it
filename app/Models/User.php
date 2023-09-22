61fc55e985bd276b781d3221175a99e55d881653
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-22 22:54:44 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,9 +10,9 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable 
+class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable ,HasRoles ,Authenticatable;
+    use HasApiTokens, HasFactory, Notifiable ,HasRoles ;
 
     /**
      * The attributes that are mass assignable.
