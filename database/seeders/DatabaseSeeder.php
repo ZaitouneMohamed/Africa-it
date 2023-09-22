2cf24de1ea08a001f72a34b397b94e4c3275b7e9
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-22 20:19:28 +0100

    small changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,10 @@ class DatabaseSeeder extends Seeder
             RolesSeeder::class,
             UsersSeeder::class,
         ]);
-        \App\Models\Branch::factory(5)->create();
-        \App\Models\Categorie::factory(5)->create();
-        \App\Models\SubCategorie::factory(30)->create();
-        \App\Models\Product::factory(60)->create();
-        \App\Models\Adresse::factory(5)->create();
+        // \App\Models\Branch::factory(5)->create();
+        // \App\Models\Categorie::factory(5)->create();
+        // \App\Models\SubCategorie::factory(30)->create();
+        // \App\Models\Product::factory(60)->create();
+        // \App\Models\Adresse::factory(5)->create();
     }
 }
