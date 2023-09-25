23f144e4101dc39c4e0df74e001dc5cda39e88d1
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-25 16:27:27 +0100

    Update HelpersIndex.php

--- app/Helpers/HelpersIndex.php
+++ app/Helpers/HelpersIndex.php
@@ -2,26 +2,23 @@
 
 function GetStarsIcon($stars)
 {
-    $stars_var = "a";
     if ($stars >= 1 && $stars < 1.5) {
-        $stars_var = `<i class='fas fa-star'></i>`;
+        return  `<i class='fas fa-star'></i>`;
     } elseif ($stars >= 1.5 && $stars < 2) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
-    }elseif ($stars >= 2 && $stars < 2.5) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i>`;
-    }elseif ($stars >= 2.5 && $stars < 3) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
-    }elseif ($stars >= 3 && $stars < 3.5) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`;
-    }elseif ($stars >= 3.5 && $stars < 4) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
-    }elseif ($stars >= 4 && $stars < 4.5) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`;
-    }elseif ($stars >= 4.5 && $stars < 5) {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
-    }else {
-        $stars_var = `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`;
+        return  `<i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
+    } elseif ($stars >= 2 && $stars < 2.5) {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i>`;
+    } elseif ($stars >= 2.5 && $stars < 3) {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
+    } elseif ($stars >= 3 && $stars < 3.5) {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`;
+    } elseif ($stars >= 3.5 && $stars < 4) {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
+    } elseif ($stars >= 4 && $stars < 4.5) {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`;
+    } elseif ($stars >= 4.5 && $stars < 5) {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i>`;
+    } else {
+        return  `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`;
     }
-    // return $stars_var;
-    return $stars;
 }
